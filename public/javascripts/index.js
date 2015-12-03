8bf9ab8a4f7ebb145cab9c8e7a8cccf6571a0abe
Ian Stewart <iastewar555@gmail.com>
2015-12-02 19:08:10 -0800

    workiing server that responds to a post request by saving the file, and sending the file to all clients. This is to be used with a file viewer client that can send a file through a post request

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,12 @@
 $(function(){
   var socket = io();
-  $('form').submit(function(){
-   socket.emit('chat message', $('#m').val());
-   $('#m').val('');
-   event.preventDefault();
-  });
+  // $('form').submit(function(){
+  //  socket.emit('chat message', $('#m').val());
+  //  $('#m').val('');
+  //  event.preventDefault();
+  // });
 
-  socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
+  socket.on('file received', function(msg){
+    $('#file').html(msg);
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var io = require('../io');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { user: req.user });
 });
+
+router.post('/', function(req, res) {
+  // console.log(Object.keys(req.body)[0]);
+
+  fs.writeFile("tmp/test.txt", Object.keys(req.body)[0], function(err) {
+      if(err) {
+          return console.log(err);
+      }
+
+      console.log("The file was saved!");
+  });
+
+  io.emit('file received', Object.keys(req.body)[0]);
+
+  res.writeHead(200);
+  res.end();
+
+})
 
 module.exports = router;
